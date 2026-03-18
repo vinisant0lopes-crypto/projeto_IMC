ff5b22234a0d93a98149c6c335663a95f80939e3
PICHAU <nomelenha@gmail.com>
2026-03-18 19:41:43 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// index 2 = peso
+// index 3 = altura
+// index 4 = imc
+
+import { calculo, imc } from "./imc.js";
+
+var trs = document.querySelectorAll('tbody tr')
+
+//laço
+
+for (let index = 0; index <=19 ; index++) {
+    let tr = trs[index]
+    let filhos = tr.children
+    
+    //Extrair informações das celulas
+    let peso = filhos[2].textContent
+    let altura = filhos[3].textContent
+    
+    //Definir como numero com virgula
+    peso = parseFloat(peso)
+    altura = parseFloat(altura)
+
+    //Realizar as funções
+    var imcconta = calculo(peso, altura)
+    var imctipo = imc(imcconta)
+
+    //Inserir o resultado
+    filhos[4].textContent = imcconta
+    filhos[5].textContent = imctipo
+
+}
+
