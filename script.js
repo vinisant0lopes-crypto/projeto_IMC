dda8cc7f9c193aef5c5592f5c24262d068d2e544
PICHAU <nomelenha@gmail.com>
2026-03-19 07:26:49 -0300

    extrair linhas da table e comentarios extras para maior contexto

--- script.js
+++ script.js
@@ -5,10 +5,12 @@
 import { calculo, imc } from "./imc.js";
 
 var trs = document.querySelectorAll('tbody tr')
+var table = document.querySelector('table')
 
 //laço
-
-for (let index = 0; index <=19 ; index++) {
+// extrai a table e aplica a propriedade .rows.lenght para obter o numero total de linhas
+for (let index = 0; index < table.rows.length ; index++) {
+    //obtem as trs e extrai seus filhos (tds)
     let tr = trs[index]
     let filhos = tr.children
     
@@ -16,7 +18,7 @@ for (let index = 0; index <=19 ; index++) {
     let peso = filhos[2].textContent
     let altura = filhos[3].textContent
     
-    //Definir como numero com virgula
+    //Definir como numero decimal
     peso = parseFloat(peso)
     altura = parseFloat(altura)
 
@@ -27,7 +29,7 @@ for (let index = 0; index <=19 ; index++) {
     //Inserir o resultado
     filhos[4].textContent = imcConta
     filhos[5].textContent = imcTipo
-
+    
 
 }
 
